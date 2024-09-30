a7c794d29f93a5b6ee8bef1b13642e94c33e8ca8
TomDan <TomDan@TOMDAN-TR-WINDO>
2024-09-30 15:08:28 +0800

    添加项目文件。

---
+++ TomDan.ToolKit/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TomDan.ToolKit.Core
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+
+        }
+    }
+}
